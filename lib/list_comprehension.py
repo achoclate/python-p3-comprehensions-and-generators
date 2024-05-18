925f4e2222c7c912f4f340efcbd37b88509bcd03
achoclate <nyabokeann99@gmail.com>
2024-05-18 13:06:29 -0700

    complete lab

--- lib/list_comprehension.py
+++ lib/list_comprehension.py
@@ -1,7 +1,19 @@
 #!/usr/bin/env python3
 
 def return_evens(num_list):
-    pass
+    return [num for num in num_list if num % 2 == 0]
+
+# Example 
+num_list = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+print(return_evens(num_list))
+
 
 def make_exclamation(sentence_list):
-    pass
+    return [sentence + '!' for sentence in sentence_list]
+
+# Example 
+sentence_list = ["Hello", "How are you", "Goodbye"]
+print(make_exclamation(sentence_list))
+
+    
+     
